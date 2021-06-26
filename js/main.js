ac47f8fb8935e4211f7270279fd373288bb06912
Marin Blažević <blazevic.marin747@gmail.com>
2021-06-26 20:50:24 +0200

    SASS, HTML % JQuery Fix

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 jQuery(function (t) {
   t("#prev").on("click", function () {
     t("#prev").attr({ disabled: !0, class: "arrow__btn--left--inactive" }),
-      t(".slider__img:first-child").hide("fast", function () {
+      t(".slider__img:first-child").hide("normal", function () {
         t(this)
           .appendTo(t(this).parent())
-          .show("fast")
+          .show("normal")
           .css({ marginRight: "10px" });
       });
 
@@ -14,10 +14,10 @@ jQuery(function (t) {
   }),
     t("#next").on("click", function () {
       t("#next").attr({ disabled: !0, class: "arrow__btn--right--inactive" }),
-        t(".slider__img:last-child").hide("fast", function () {
+        t(".slider__img:last-child").hide("normal", function () {
           t(this)
             .prependTo(t(this).parent())
-            .show("fast")
+            .show("normal")
             .css({ marginLeft: "10px" });
         });
       setTimeout(() => {
